859b3713cefd6746ab77c93407c0ad8debef6e3a
Philiuh <piliuh3@gmail.com>
2019-03-17 16:23:08 +0300

    zoom+draw implemented

--- CameraApp/app/src/main/java/com/example/cameraapp/DisplayImage.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/DisplayImage.java
@@ -2,101 +2,119 @@ package com.example.cameraapp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.github.chrisbanes.photoview.OnPhotoTapListener;
+import com.github.chrisbanes.photoview.PhotoView;
+import java.util.ArrayList;
 
 public class DisplayImage extends AppCompatActivity {
 
-    ImageView imageView;
+    PhotoView mPhotoView;
     Bitmap workingbitmap, mutablebitmap;
-    TextView tvMark, tvModel, tvPosition;
+    TextView tvMark, tvModel, tvPosition, tvMenu;
+    Paint paint = new Paint();
+    Path path;
+    Canvas canvas;
 
-    final int MENU_POSITION_FRONT = 1;
-    final int MENU_POSITION_BACK = 2;
-    final int MENU_POSITION_FRONT_LEFT = 3;
-    final int MENU_POSITION_FRONT_RIGHT = 4;
+    private float x_start, y_start;
+    ArrayList<Float> x_dots = new ArrayList<>();
+    ArrayList<Float> y_dots = new ArrayList<>();
+    private int i = 0;
 
-    final int MENU_MARK_LADA = 5;
-    final int MENU_MARK_VAZ = 6;
-    final int MENU_MARK_MARUSSIA = 7;
-    final int MENU_MARK_VOLGA = 8;
-    final int MENU_MARK_ZIL = 9;
-    final int MENU_MARK_MOSKVICH = 10;
-    final int MENU_MARK_GAZ = 11;
-    final int MENU_MARK_UAZ = 12;
+    final int MENU_POSITION_FRONT = 3;
+    final int MENU_POSITION_BACK = 4;
+    final int MENU_POSITION_FRONT_LEFT = 5;
+    final int MENU_POSITION_FRONT_RIGHT = 6;
 
-    final int MENU_MARK_LADA_KALINA = 13;
-    final int MENU_MARK_LADA_SAMARA = 14;
-    final int MENU_MARK_LADA_PRIORA = 15;
-    final int MENU_MARK_LADA_VESTA = 16;
-    final int MENU_MARK_LADA_XRAY = 17;
-
-    final int TEST = 18;
+    final int MENU_CLEAN_CANVAS = 1;
+    final int MENU_NEW_OBJECT = 2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_image);
-        imageView = findViewById(R.id.mimageView);
+        mPhotoView = findViewById(R.id.mimageView);
         workingbitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
         mutablebitmap = workingbitmap.copy(Bitmap.Config.ARGB_8888, true);
-        imageView.setImageBitmap(mutablebitmap);
+
+        mPhotoView.setImageBitmap(mutablebitmap);
+        canvas = new Canvas(mutablebitmap);
+        mPhotoView.setOnPhotoTapListener(new PhotoTapListener());
 
         tvMark = findViewById(R.id.markView);
         tvModel = findViewById(R.id.modelView);
         tvPosition = findViewById(R.id.positionView);
+        tvMenu = findViewById(R.id.actionMenu);
 
         registerForContextMenu(tvMark);
         registerForContextMenu(tvModel);
         registerForContextMenu(tvPosition);
+        registerForContextMenu(tvMenu);
 
+        paint.setColor(Color.rgb(0,255,0));
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStrokeWidth(10);
+
+    }
+
+    public class PhotoTapListener implements OnPhotoTapListener {
+
+        @Override
+        public void onPhotoTap(ImageView view, float x, float y) {
+
+            Log.wtf("TAG","Kd pressed: x [" + x * mPhotoView.getWidth() + "] y [" + y * mPhotoView.getHeight() + "]");
+            Log.wtf("TAG","Width: " + mPhotoView.getWidth() + " Heigth: " + mPhotoView.getHeight());
+            x_dots.add(x * canvas.getWidth());
+            y_dots.add(y * canvas.getHeight());
+            onDrawForeground(canvas);
+            mPhotoView.invalidate();
+        }
+    }
+
+    public void onDrawForeground(Canvas canvas) {
+
+        Log.wtf("TAG", "start drawing");
+        canvas.drawCircle(x_dots.get(i),y_dots.get(i),10,paint);
+
+        if (x_dots.size() > 1) {
+            while (i < x_dots.size() - 1) {
+                canvas.drawCircle(x_dots.get(i + 1),y_dots.get(i + 1),10,paint);
+                canvas.drawLine(x_dots.get(i), y_dots.get(i), x_dots.get(i + 1), y_dots.get(i + 1), paint);
+                i++;
+            }
+        }
+        else canvas.drawCircle(x_dots.get(i),y_dots.get(i),10,paint);
+        mPhotoView.onDrawForeground(canvas);
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         // TODO Auto-generated method stub
         switch (v.getId()) {
-            case (R.id.markView):
-                menu.add(0, MENU_MARK_LADA, 0, "Лада");
-                menu.add(0, MENU_MARK_VAZ, 0, "ВАЗ");
-                menu.add(0, MENU_MARK_MARUSSIA, 0, "Marussia");
-                menu.add(0, MENU_MARK_VOLGA, 0, "Волга");
-                menu.add(0, MENU_MARK_ZIL, 0, "ЗИЛ");
-                menu.add(0, MENU_MARK_MOSKVICH, 0, "Москвич");
-                menu.add(0, MENU_MARK_GAZ, 0, "ГАЗ");
-                menu.add(0, MENU_MARK_UAZ, 0, "УАЗ");
-                break;
-            case (R.id.modelView):
-                if (tvMark.getText().toString() == "Лада")
-                    menu.add(0,MENU_MARK_LADA_KALINA,0,"Калина");
-                if (tvMark.getText().toString() == "Лада")
-                    menu.add(0,MENU_MARK_LADA_SAMARA,0,"Самара");
-                if (tvMark.getText().toString() == "Лада")
-                    menu.add(0,MENU_MARK_LADA_PRIORA,0,"Приора");
-                if (tvMark.getText().toString() == "Лада")
-                    menu.add(0,MENU_MARK_LADA_VESTA,0,"Веста");
-                if (tvMark.getText().toString() == "Лада")
-                    menu.add(0,MENU_MARK_LADA_XRAY,0,"XRay");
-                else
-                    menu.add(0,TEST,0,"Сперва выберите марку");
 
-                break;
             case (R.id.positionView):
                 menu.add(0,MENU_POSITION_FRONT,0,"Спереди");
                 menu.add(0,MENU_POSITION_BACK,0,"Сзади");
                 menu.add(0,MENU_POSITION_FRONT_LEFT,0,"Перед налево");
                 menu.add(0,MENU_POSITION_FRONT_RIGHT,0,"Перед направо");
                 break;
+            case (R.id.actionMenu):
+                menu.add(0,MENU_CLEAN_CANVAS,0,"Очистить изображение");
+                menu.add(0,MENU_NEW_OBJECT,0,"Добавить объект");
         }
     }
-
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
@@ -115,59 +133,23 @@ public class DisplayImage extends AppCompatActivity {
             case MENU_POSITION_FRONT_RIGHT:
                 tvPosition.setText("Перед направо");
                 break;
-            case MENU_MARK_LADA:
-                tvMark.setText("Лада");
+            case MENU_CLEAN_CANVAS:
+                x_dots.clear();
+                y_dots.clear();
+                canvas = null;
+                workingbitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
+                mutablebitmap = workingbitmap.copy(Bitmap.Config.ARGB_8888, true);
+                mPhotoView.setImageBitmap(mutablebitmap);
+                canvas = new Canvas(mutablebitmap);
+                paint.setColor(Color.rgb(0,255,0));
+                paint.setStyle(Paint.Style.FILL_AND_STROKE);
+                paint.setStrokeWidth(10);
+                i = 0;
                 break;
-            case MENU_MARK_VAZ:
-                tvMark.setText("ВАЗ");
-                break;
-            case MENU_MARK_MARUSSIA:
-                tvMark.setText("Marussia");
-                break;
-            case MENU_MARK_VOLGA:
-                tvMark.setText("Волга");
-                break;
-            case MENU_MARK_ZIL:
-                tvMark.setText("ЗИЛ");
-                break;
-            case MENU_MARK_MOSKVICH:
-                tvMark.setText("Моквич");
-                break;
-            case MENU_MARK_GAZ:
-                tvMark.setText("ГАЗ");
-                break;
-            case MENU_MARK_UAZ:
-                tvMark.setText("УАЗ");
-                break;
-            case MENU_MARK_LADA_KALINA:
-                tvModel.setText("Калина");
-                break;
-            case MENU_MARK_LADA_PRIORA:
-                tvModel.setText("Приора");
-                break;
-            case MENU_MARK_LADA_VESTA:
-                tvModel.setText("Веста");
-                break;
-            case MENU_MARK_LADA_SAMARA:
-                tvModel.setText("Самара");
-                break;
-            case MENU_MARK_LADA_XRAY:
-                tvModel.setText("XRay");
+            case MENU_NEW_OBJECT:
+                i++;
                 break;
         }
         return super.onContextItemSelected(item);
-    }
-
-
-    public boolean onTouchEvent(MotionEvent event) {
-
-        int action = event.getAction();
-        switch (action) {
-
-            case MotionEvent.ACTION_DOWN:
-                imageView.getLayoutParams().width = 500;
-                imageView.getLayoutParams().height = 500;
-        }
-        return super.onTouchEvent(event);
     }
 }
--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -46,9 +47,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayImage(View view) {
-        Intent intent = new Intent(this,DisplayImage.class);
-        intent.putExtra("image_path",currentImagePath);
-        startActivity(intent);
+            Intent intent = new Intent(this, DisplayImage.class);
+            intent.putExtra("image_path", currentImagePath);
+            startActivity(intent);
     }
 
     private File getImageFile() throws IOException {
